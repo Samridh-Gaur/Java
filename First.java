3958ec2ebfc2e093c8fdb90b1c67dbed5cbde668
Samridh-Gaur <samridhgaur2005@gmail.com>
2025-09-30 13:03:14 +0530

    1st java code

---
+++ First.java
@@ -0,0 +1,32 @@
+public class First
+{
+    static int a = 100; //This is the static variable
+    public static void main(String[] args) 
+    {
+       System.out.println("Your number is " +First.a);
+       a = 200;
+       System.out.println("Now the number is " +First.a);
+    }
+}
+
+class Second
+{
+    int a = 100; //This is the instance variable
+
+    public static void main(String[] args) 
+    {
+        First abc = new First();
+        System.out.println("Interest is " +abc.a);
+    }
+}
+
+class third
+{
+    static boolean a;
+
+    public static void main(String[] args) {
+        System.out.println(third.a);
+    }
+}
+
+
