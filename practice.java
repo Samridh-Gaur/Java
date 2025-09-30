6b1047e7cc41c6ecaff1ca86715b73cbc3f05445
Samridh-Gaur <samridhgaur2005@gmail.com>
2025-09-30 16:48:33 +0530

    operators

---
+++ practice.java
@@ -0,0 +1,8 @@
+public class practice {
+    public static void main(String[] args) {
+        double d = 5.5555;
+        int i = (int) d;
+        System.out.println(d);
+        System.out.println(i);
+    }
+}
