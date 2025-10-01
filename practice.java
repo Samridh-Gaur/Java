7564d789d38c898eafccf564640017b56d3eb362
Samridh-Gaur <samridhgaur2005@gmail.com>
2025-10-01 18:23:31 +0530

    Loops

--- practice.java
+++ practice.java
@@ -1,8 +1,54 @@
-public class practice {
-    public static void main(String[] args) {
-        double d = 5.5555;
-        int i = (int) d;
-        System.out.println(d);
-        System.out.println(i);
+public class practice 
+{
+    public static void main(String[] args) 
+    {
+        // double d = 5.5555;
+        // int i = (int) d;
+        // System.out.println(d);
+        // System.out.println(i);
+        // int a = 10 , b = 20;
+        // // int c = ++a;
+        // // c = a++;
+        // // System.out.println("a is "+a);
+        // // System.out.println("c is " +c);
+
+        // boolean varia = a < b;
+        // System.out.println(varia);
+
+        // int a = 10;
+        // double d = a;
+        // int b = (int) d;
+        // System.out.println(b);
+
+        // int c = (a*b);
+        // // System.out.println(c);
+
+
+        // int a = 0;
+
+        // if(a>0)
+        // {
+        //     System.out.println("bhai number positive ha,meri baat maan");
+        // }
+
+        // else
+        // {
+        //     System.out.println("bhai number negative ha");
+        // }
+
+
+        // 
+        // 
+        
+        // for(long i = 0;i<11;i++)
+        // {
+        //     System.out.println("value of i is "+i);
+        // }
+        int i = 10;
+        while (i>0) {
+            System.out.println("value of i is "+i);
+            i--;
+        }
+        
     }
 }
