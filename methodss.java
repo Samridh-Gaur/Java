a6a88b4fd2796ccfc68d9a161961cb7f946c785f
Samridh-Gaur <samridhgaur2005@gmail.com>
2025-10-16 17:16:20 +0530

    class, objects and variables

---
+++ methodss.java
@@ -0,0 +1,46 @@
+import java.sql.Wrapper;
+import java.util.Scanner;
+
+public class methodss 
+{
+
+    int a = 10;
+
+    static int add(int a , int b)
+    {
+        return a+b;
+    }
+    public static void main(String[] args) 
+    {
+        
+
+        // 1> Write a Java program to create a Class and Object.
+        // methodss obj = new methodss();
+        // System.out.println(obj.a);
+
+        // 2> Write a program using Instance Variables and Class Variables to calculate the marks of a student in five subjects.
+        // Skip
+
+        // 3>Write a Java Program to perform addition, subtraction, and multiplication of two values using the parameterised method. 
+        // int ans = add(100, 200);
+        // System.out.println(ans);
+
+
+        // 4> Write a program to check whether a number is even or odd using the Parametrised Method in Java 
+
+        // 5> Write a Java program to display the names of Students and calculate the ages of five students in a class using the Non-Parametrised Method. 
+
+        // 6> Write a program to initialise an object using a reference variable 
+
+        // 7> Write a program in Java to find the square of two numbers using the anonymous objects method. 
+
+        // 8> Imagine a Student Marks Processing System, where:
+        // Marks are entered as Strings 
+        // The program converts them into integers for calculations.
+        // Then, calculate the Average and Total Marks using the Unboxing feature 
+        // Using the Wrapper Class method Autoboxing calculates if a student passed (the average should be greater than 50 for passing).
+
+        
+    }
+
+}
