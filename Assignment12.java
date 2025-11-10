b65805e028775f0b49e2adc16e4c843018b936c8
Samridh-Gaur <samridhgaur2005@gmail.com>
2025-11-10 13:01:09 +0530

    Assignment 12

--- Assignment12.java
+++ Assignment12.java
@@ -1,7 +1,65 @@
+// class bike
+// {
+//     void run()
+//     {
+//         System.out.println("this is a bike");
+//     }
+// }
+
+// class sp extends bike
+// {
+//     void run()
+//     {
+//         System.out.println("sp bike");
+//     }
+// }
+
+// public class Assignment12 
+// {
+//     public static void main(String[] args) 
+//     {
+//         bike abc = new sp();  // Upcasting
+//         abc.run();
+//     }
+// }
+
+// class bank
+// {
+//     private int balance = 100000;
+
+//     void deposit(int x)
+//     {
+//         balance += x;
+//     }
+
+//     void CB()
+//     {
+//         System.out.println(balance);
+//     }
+// }
+
+
+class roshit
+{
+    void r()
+    {
+        System.out.println("Hi brother");
+    }
+}
+
+class samridh extends roshit
+{
+    void r()
+    {
+        System.out.println("bhai ha tu");
+    }
+}
+
 public class Assignment12 
 {
     public static void main(String[] args) 
     {
-        
+        samridh abc = new samridh();
+        abc.r();
     }
 }
